a74bb19ebf50a9c90c90745ba64e803fba301baa
Mark Wilde <markwilde29@gmail.com>
2020-05-12 13:20:01 +0100

    Created a function to load events, created a function to add a task.

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
 // Define UI Vars
 const form = document.querySelector("#task-form");
 const taskList = document.querySelector(".collection");
-const taskList = document.querySelector(".clear-tasks");
+const clearBtn = document.querySelector(".clear-tasks");
 const filter = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
+
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener("submit", addTask);
+}
+
+// Call the function
+loadEventListeners();
+
+// Add Task
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Enter a task to add");
+  } else {
+    // Create li element
+    const li = document.createElement("li");
+    // Add class
+    li.className = "collection-item";
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element
+    const link = document.createElement("a");
+    // Add class
+    link.className = "delete-item secondary-content";
+    // Add icon html
+    link.innerHTML = "<i class='fa fa-remove'> </i>";
+    // Append the link to the li
+    li.appendChild(link);
+
+    // Append li to ul
+    taskList.appendChild(li);
+
+    // Clear input
+    taskInput.value = "";
+  }
+  e.preventDefault();
+}
